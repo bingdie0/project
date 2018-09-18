41ba2362999aafbede25441483e0b64873c33f36
WANGJ <2794405068@qq.com>
2018-09-18 17:42:23 +0800

    修改pom文件

--- project-rest/src/main/java/com/my/project/ProjectApplication.java
+++ project-rest/src/main/java/com/my/project/ProjectApplication.java
@@ -1,17 +1,18 @@
 package com.my.project;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author: Mr.WangJie
  * @date: 2018-09-07
  **/
 @ComponentScan({"com.my.framework", "com.my.project"})
-@MapperScan("com.my.project.dao")
+@MapperScan({"com.my.framework.datasource.service", "com.my.project.dao"})
 @EnableWebMvc
 @SpringBootApplication
 public class ProjectApplication {
